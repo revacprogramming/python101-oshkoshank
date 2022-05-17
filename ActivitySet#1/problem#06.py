db48c09146b274a2ae98b4b0bfdca912deeb9a4b
oshkoshank <bandstars45@gmail.com>
2022-05-17 16:05:58 +0530

    commit 5

--- ActivitySet#1/problem#06.py
+++ ActivitySet#1/problem#06.py
@@ -1,16 +1,23 @@
-# Loops & Iterators
+"""Write a program that repeatedly prompts a user for integer numbers until the user enters 'done'. Once 'done' is entered, print out the largest and smallest of the numbers. If the user enters anything other than a valid number catch it with a try/except and put out an appropriate message and ignore the number. Enter 7, 2, bob, 10, and 4 and match the output below."""
 
-largest = None
-smallest = None
+l = None
+s = None
 
 while True:
-    num = input("Enter a number? ")
+    n =input("Enter your numbers: ")
+    if n == 'done':
+       break
+    try:
+        n = int(n)
+        if l is None or n > l:
+            l = n
+        if s is None or s > n:
+              s = n
 
-    if num == "done":
-        break
+    except:
+           print("Shut up and enter a proper number you idiot !!!!")
+           
+print("The largest number amount you entered is ",l)
+print("The smallest number amount you entered is ",s)
 
-    # ...
 
-    print(num)
-
-print("Maximum", largest)
