9abd7d160244b6438a6099854d279ff80694d0be
oshkoshank <bandstars45@gmail.com>
2022-05-24 19:46:03 +0530

    commit 8

--- ActivitySet#1/problem#07.py
+++ ActivitySet#1/problem#07.py
@@ -1,2 +1,5 @@
-text = "X-DSPAM-Confidence:    0.8475"
-print(float(text[text.find('0'):]))
+def new_func():
+    text = "X-DSPAM-Confidence:    0.8475"
+    print(float(text[text.find('0'):]))
+
+new_func()
