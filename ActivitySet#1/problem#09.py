b920c158340d81f02695a78a4518e8b8b516f90c
Ankush Hegde <97220529+oshkoshank@users.noreply.github.com>
2022-05-26 21:11:25 +0530

    commit 10

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,14 @@
-# Lists
+fhand = open("romeo.txt")
 
-filename = "dataset/romeo.txt"
+lst = list()
+
+for line in fhand:
+    line = line.rstrip()
+    line = line.split()
+    for i in line:
+        if i not in lst:
+            lst.append(i)
+
+lst.sort()
+    
+print(lst)
