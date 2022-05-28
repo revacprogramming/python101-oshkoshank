e1b799051a7271ddb72aae0cb7dd0515dc9ffba4
oshkoshank <bandstars45@gmail.com>
2022-05-28 16:15:54 +0530

    commit

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,13 +1,9 @@
-fhand = open("mbox-short.txt")
-count = 0
-for line in fhand:
+fh = open('romeo.txt','r')
+l = []
+for line in fh:
     line = line.rstrip()
-    if line == "": continue
-        
-    words = line.split()
-    if words[0] !="From": continue
-        
-    print(words[1])
-    count = count+1
-
-print ("There were", count, "lines in the file with From as the first word")
+    line = line.split(' ')
+    for word in line:
+        if word not in l:
+            l.append(word)
+print(sorted(l))
