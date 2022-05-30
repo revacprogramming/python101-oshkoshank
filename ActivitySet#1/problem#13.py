138eac24bc6329b2130d550c3ee70bcd63940ede
oshkoshank <bandstars45@gmail.com>
2022-05-30 21:12:17 +0530

    commit

--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -1,2 +1,9 @@
-# Network Programming
-# https://www.py4e.com/lessons/network
+import re
+file = open("data.txt")
+regex = '\d+'
+sum = 0
+for l in file:
+    lst = re.findall(regex,l)
+    for i in lst:
+        sum += int(i)
+print(sum)
