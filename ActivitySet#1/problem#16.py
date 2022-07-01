ca7f9de06c1d66c104d507c127d7754a7709f96e
oshkoshank <bandstars45@gmail.com>
2022-07-01 09:01:58 +0530

    commit

--- ActivitySet#1/problem#16.py
+++ ActivitySet#1/problem#16.py
@@ -18,3 +18,28 @@ for i in range(count):
     html = urlopen(link).read()
     soup = BeautifulSoup(html,"html.parser")
     href = soup('a')
+
+
+
+
+"""from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+import re
+
+req = Request("http://py4e-data.dr-chuck.net/known_by_Ceitidh.html")
+html_page = urlopen(req)
+
+
+soup = BeautifulSoup(html_page,"lxml") 
+lnk = []
+for i in soup.find_all('a'):
+    lnk.append(i.get('href'))
+for i in range(7):
+  req = Request(lnk[17])
+  html_page = urlopen(req)
+  soup = BeautifulSoup(html_page,"lxml")
+  for title in soup.find_all('title'):
+    print(title.get_text())
+  lnk.clear()
+  for i in soup.find_all('a'):
+    lnk.append(i.get('href')"""
