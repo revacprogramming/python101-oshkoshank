14224421d459e630750aa609cafc855c9eade749
oshkoshank <bandstars45@gmail.com>
2022-07-04 22:20:04 +0530

    commit

--- ActivitySet#1/problem#23.py
+++ ActivitySet#1/problem#23.py
@@ -1,8 +1,8 @@
 class test:
     def calculate(self,x,y):
-        print(f"The price is {x*y}")
+        print(f"The price is {x*y}");
 
 obj = test()
-obj.price = 100
+obj.price = 100;
 obj.quantity = 5;
-obj.calculate(obj.price,obj.quantity)
+obj.calculate(obj.price,obj.quantity);
