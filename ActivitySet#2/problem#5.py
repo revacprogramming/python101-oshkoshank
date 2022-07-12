3efcdac441cbdd31a9e1e4bd8b10d19cc38a5c28
oshkoshank <bandstars45@gmail.com>
2022-07-12 08:32:23 +0530

    commit

--- ActivitySet#2/problem#5.py
+++ ActivitySet#2/problem#5.py
@@ -1,25 +1,29 @@
-
-
 def get_cs():
-    """get string input"""
+  k=input("Enter the credentials:- ")
+  return k
 
 
 def cs_to_dict(cs):
-    """convert connect string to a dictionary"""
+  d=dict()
+  k=cs.split(";")
+  for i in k:
+    j=i.split("=")
+    d[j[0]]=j[1]
+  return d
 
 
 def dict_to_cs(d):
-    """convert a dictionary to connect string"""
+  for i,j in d.items():
+    print(i,"=",j,end=";")
+    # print(d.keys(),d.values())
 
 
 def main():
-    cs = get_cs()
-
-    d = cs_to_dict(cs) # convert connect string to a dictionary
+    cs=get_cs()
+    d=cs_to_dict(cs) 
     print(d)
-
     cs = dict_to_cs(d)
-    print(cs)
+    # print(cs)
 
 
 if __name__ == '__main__':
