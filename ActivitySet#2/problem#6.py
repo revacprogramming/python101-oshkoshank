28e44251f936ac0eca5278b909ca6f34dbf04817
oshkoshank <bandstars45@gmail.com>
2022-07-13 21:09:29 +0530

    commit

--- ActivitySet#2/problem#6.py
+++ ActivitySet#2/problem#6.py
@@ -1,11 +1,16 @@
+class Menu(dict):
+  def __init__(self):
+    self=dict()
+
+  def add(self,item,price):
+    self[item]=price
+
+  def show(self):
+    for i,j in self.items():
+        print(i,j)
 
 
-class Menu:
-    """fill in class definition here"""
-
-
-m = Menu()  # Menu is a class
-m.add("idly", 10)
-m.add("vada". 20)
-
+m = Menu()
+m.add('idly', 10)
+m.add('vada', 20)
 m.show()
